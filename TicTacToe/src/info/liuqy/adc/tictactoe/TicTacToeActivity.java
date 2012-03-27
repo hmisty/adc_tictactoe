093295d04d87c6d63389c5e4e0ab4988ef30480a
Evan Liu L <evan.l.liu@ericsson.com>
2012-03-27 22:21:24 +0800

    newly created

---
+++ TicTacToe/src/info/liuqy/adc/tictactoe/TicTacToeActivity.java
@@ -0,0 +1,13 @@
+package info.liuqy.adc.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TicTacToeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
