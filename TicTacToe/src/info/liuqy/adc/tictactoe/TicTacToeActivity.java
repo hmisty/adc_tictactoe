bcd44d4662f6d46e00ba6cafee9aa403be47e5ea
Evan Liu L <evan.l.liu@ericsson.com>
2012-03-27 22:28:00 +0800

    impl activity show gridview. finished.

--- TicTacToe/src/info/liuqy/adc/tictactoe/TicTacToeActivity.java
+++ TicTacToe/src/info/liuqy/adc/tictactoe/TicTacToeActivity.java
@@ -2,6 +2,11 @@ package info.liuqy.adc.tictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class TicTacToeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,15 @@ public class TicTacToeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        GridView grid = (GridView) this.findViewById(R.id.gridview);
+        grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                ImageView iv = (ImageView) v;
+                iv.setImageResource(R.drawable.x);
+            }
+        });
+
     }
 }
